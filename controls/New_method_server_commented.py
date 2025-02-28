0b2e2755a8563367c21bc91da203f749d215f004
Drep2718 <aidendrep@icloud.com>
2025-02-27 20:11:03 -0500

    Commented Through

--- controls/New_method_server_commented.py
+++ controls/New_method_server_commented.py
@@ -13,8 +13,8 @@ TIME = 0.5 #Constant for time
 def connect_ball(toy, sb_list): # attepts to connect to a sphero 
     sb = 0
     try:
-        sb = SpheroEduAPI(toy).__enter__()
-        sb_list.append(sb)
+        sb = SpheroEduAPI(toy).__enter__() #establishes the connetion
+        sb_list.append(sb) # add it to the list of connected balls 
         print(sb)
     except Exception:
         print("Connection Failure")
@@ -22,63 +22,64 @@ def connect_ball(toy, sb_list): # attepts to connect to a sphero
 def terminate_ball(sb):
     sb.__exit__(None, None, None)
 
-def run_command(instruction, sb):
-    global on
+def run_command(instruction, sb): #robot and Intruction which is an object of commands
+    global on #Varible to signify the start and end of program, used to shut down when turned off
+              #Only tunred on at begining of program 
     print("{}: {}".format(sb._SpheroEduAPI__toy, instruction.type))
     match (instruction.type):
         case 0:
-            sb.set_main_led(instruction.color)
+            sb.set_main_led(instruction.color) #set main led, getting data from loaded in insturction through pickel
         case 1:
-            sb.roll(sb.get_heading(), instruction.speed, instruction.duration)
+            sb.roll(sb.get_heading(), instruction.speed, instruction.duration)# gets parameters for roll command from pickel
         case 2:
-            sb.spin(instruction.degrees, instruction.duration)
+            sb.spin(instruction.degrees, instruction.duration)#gets parameters for roll command from pickel
         case 3:
-            on = False
+            on = False # set to false to know to terminate the program
 
-def control():
+def control(): #socket server 
     global on
-    lock = Lock()
-    s = socket.socket()
-    port = 1234
-    s.bind(('localhost', port))
-    s.listen(5)
+    lock = Lock() #Initialize the lock
+    s = socket.socket() #creates the socket
+    port = 1234 #port number used to establish a connection
+    s.bind(('localhost', port))  
+    s.listen(5) #listens for a connection to the socekt
     print("Waiting for connection")
-    c, address = s.accept()
+    c, address = s.accept() 
     while (on):
         try:
-            instructionList = pickle.loads(c.recv(1024))
+            instructionList = pickle.loads(c.recv(1024)) #pickel used to serilaze the insturctions from the client 
         except EOFError:
             print("EOFError")
-            s.close()
+            s.close()#closes the connection
             on = False
         try:
-            lock.acquire()
+            lock.acquire() #lock this part 
             for instruction in instructionList:
-                if (instruction.type == 3):
+                if (instruction.type == 3): #signifies breaking out of the loop after the command is recieved
                     on = False
                     print("TERMINATION COMMAND RECIEVED")
-                if (not (instruction.spheroID < len(command_arr))):
-                    print("index out of bounds")
+                if (not (instruction.spheroID < len(command_arr))): #makes sure that you are not acessing a command for a sphero that does not exist
+                    print("index out of bounds") 
                 else:
-                    command_arr[instruction.spheroID].append(instruction)
+                    command_arr[instruction.spheroID].append(instruction) #appends the instruction to the command array at the sheros index
         finally:
             lock.release()
 
-toy_names = ["SB-B5A9", "SB-1840", "SB-76B3", "SB-E274", "SB-BD0A"]
+toy_names = ["SB-B5A9", "SB-1840", "SB-76B3", "SB-E274", "SB-BD0A"] #specefied toy names 
 toys = []
 attempts = 0
-while (len(toys) != len(toy_names) and attempts < 5):
+while (len(toys) != len(toy_names) and attempts < 5): #ensures that all spheros are found and connected
     toys = scanner.find_toys(toy_names=toy_names)
     attempts += 1
 print(toys)
-if (len(toys) != len(toy_names) and attempts >= 5):
+if (len(toys) != len(toy_names) and attempts >= 5): #if not than throw a runtime error 
     raise RuntimeError("Not all balls actually connected")
 sb_list = []
 
 global command_arr
 command_arr = []
-for i in range(0, len(toys), 1):
-    command_arr.append([])
+for i in range(0, len(toys), 1): #from 0 to how many toys you have conneted to
+    command_arr.append([]) #appends an empty array to the command array to keep track of all the spheros 
 global on
 on = True
 
