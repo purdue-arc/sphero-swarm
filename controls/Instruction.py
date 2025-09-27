8d767b76debba9949fe9cfdbedd0d9d413868061
zhangPurdueARC <144553407+zhangPurdueARC@users.noreply.github.com>
2025-09-27 16:16:14 -0400

    Update Instruction.py

--- controls/Instruction.py
+++ controls/Instruction.py
@@ -1,10 +1,9 @@
 from spherov2.types import Color
 
-
 class Instruction:
     """
     type:
-     -1 = kill all spheros
+      -1 = kill all spheros
       0 = reset LED color
       1 = roll
       2 = turn
@@ -19,25 +18,21 @@ class Instruction:
     duration = 0
 
     def __init__(self, *args):
-        if (args[1] == 0):
-            self.spheroID = args[0]
-            self.type = args[1]
-            self.color = args[2]
-        
-        elif (args[1] == 1):
-            self.spheroID = args[0]
-            self.type = args[1]
-            self.speed = args[2]
-            self.duration = args[3]
-
-        elif (args[1] == 2):
-            self.spheroID = args[0]
-            self.type = args[1]
-            self.degrees = args[2]
-            self.duration = args[3]
-
-        elif (args[1] == 3):
-            self.type = 3
-
-        elif (args[1] == -1):
-            self.type = -1
+        self.spheroID = args[0]
+        self.type = args[1]
+        match (args[1]):
+            case -1:
+                # valid case, but this will just make a terminate command
+                pass
+            case 0:
+                self.color = args[2]
+            case 1:
+                self.speed = args[2]
+                self.duration = args[3]
+            case 2:
+                self.degrees = args[2]
+                self.duration = args[3]
+            case 3:
+                self.duration = args[2]
+            case _:
+                raise ValueError("Don't enter values that aren't -1 through 4 please")
