4e6b1a4c0a12f976fed0a2451bde977a663538f4
alankang30 <alankang30@gmail.com>
2025-10-04 16:02:30 -0400

    created SpheroCoordinate class

--- perception/sphero_spotter.py
+++ perception/sphero_spotter.py
@@ -3,11 +3,16 @@ import pickle
 import zmq # for socket to connect to algs
 import time
 
+from SpheroCoordinate import SpheroCoordinate
+
 # CONSTANTS
 GRID_DIM_X = 12 # TODO actually like make these real. right now they are all made up 
 GRID_DIM_Y = 10
 frame_dim_x = 1080
 frame_dim_y = 1440
+
+# Global array of SpheroCoordinates
+spheros = []
 
 def pixel_to_grid_coords(pixel_x, pixel_y):
     '''
@@ -18,13 +23,14 @@ def pixel_to_grid_coords(pixel_x, pixel_y):
     return None
 # 
 
-def get_sphero_coords():
+def initialize_spheros():
     '''
-    TODO 
-    Returns list of coords of all spheros in terms of grid units.
+    TODO @anthony
+    initializes global sphero object array.
+    Returns number of spheros found.
     '''
-    coords = []
-    return coords
+    n_found = 0
+    return n_found
 
 def listener():
     '''
@@ -48,16 +54,20 @@ def listener():
         print(f"Received request '{msg}' from algorithms!")
 
         if msg == 'init':
-            socket.send_string("connected")
+            num_found = initialize_spheros() # get their positions and assign IDs.
+            socket.send_string(f"connected to {num_found} spheros")
+
         elif msg == 'coords':
             #reply = pickle.dump(get_sphero_coords())    # byte dump list of spheros. Algorithms gets to unpack it
             #reply = pickle.dump('hi')
             #socket.send(reply)                   # send bytedump of sphero coord objects back
             socket.send_string('[pickled SpheroCoordinate objects]')
             print('sphero_spotter: sending coords')
+
         elif msg == 'exit':
             socket.send_string('bye')
             break
+
         else:
             socket.send_string("error - command doesn\'t match one of ['init', 'coords', 'exit']")
 
@@ -73,5 +83,10 @@ if __name__ == '__main__':
     thread.start()
 
     # TODO start the camera feed, object tracking and updating, all that stuff
+
+    # start camera feed
+
+
+
 
 
